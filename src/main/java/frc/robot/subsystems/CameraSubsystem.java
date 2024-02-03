40cea5f78648b4fef27178ab7bdc6c12f4909191
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-02-03 18:08:32 -0500

    added some constants and distance from tag

--- src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -7,17 +7,34 @@ package frc.robot.subsystems;
 import java.util.List;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 
 public class CameraSubsystem extends SubsystemBase {
   public PhotonPipelineResult result;
   public final PhotonCamera m_camera;
-  private double targetAngle = 0;
+  private final double CameraHeightMetres = 0.44;
+  private final double TargetHeightMetres = 1.35;
+  private final double CameraPitchRadians = 0.17;
+  private double tagDistance = 0;
+  private double tagAngle = 0;
   private int ID;
+
+  //simulation stuff
+
+  double camDiagFOV = 68.5; // degrees - assume wide-angle camera
+  double camPitch = 0.17; // degrees
+  double camHeightOffGround = 0.44; // meters
+  double maxLEDRange = 20; // meters
+  int camResolutionWidth = 1280; // pixels
+  int camResolutionHeight = 720; // pixels
+  double minTargetArea = 10; // square pixels
 //test
   public CameraSubsystem(PhotonCamera camera) {
     m_camera = camera;
@@ -31,19 +48,21 @@ public class CameraSubsystem extends SubsystemBase {
     PhotonTrackedTarget target = result.getBestTarget();
 
     if(hasTargets){
-      targetAngle = target.getYaw();
+      tagAngle = target.getYaw();
       ID = target.getFiducialId();
-
+      tagDistance = PhotonUtils.calculateDistanceToTargetMeters(CameraHeightMetres, TargetHeightMetres, CameraPitchRadians, Units.degreesToRadians(result.getBestTarget().getPitch()));
     }
 
 
-    SmartDashboard.putNumber("AprilTag target angle", targetAngle);
+    SmartDashboard.putNumber("AprilTag target angle", tagAngle);
     SmartDashboard.putBoolean("Has targets", result.hasTargets());
     SmartDashboard.putNumber("Apriltag ID", ID);
+    SmartDashboard.putNumber("Apriltag distance",tagDistance);
+
   }
 
   public double returnTargetYaw(){
-    return targetAngle;
+    return tagAngle;
   }
 
 }
